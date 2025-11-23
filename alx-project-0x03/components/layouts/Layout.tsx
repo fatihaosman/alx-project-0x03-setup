007eb42b207104949a406515dbc7af61da893c71
fatihaosman <fatihaosman17@gmail.com>
2025-11-23 18:40:48 +0300

    solving the interface errors

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,7 +1,8 @@
+import {LayoutProps} from "@/interface";
 import { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import {LayoutProps} from "@/interface";
+
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
