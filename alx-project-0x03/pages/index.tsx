063c7e5211eb16809d8bb786ba23eb78f3f5734a
fatihaosman <fatihaosman17@gmail.com>
2025-11-23 18:17:25 +0300

    added 404.tsx, upd

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,11 +1,4 @@
-// const Home = () => {
-//   return (
-//    <div>
-//     <h1> Welcome to Splash App </h1>
-//    </div>)
-// }
 
-// export default Home;
 
 import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
